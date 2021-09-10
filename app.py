64fbc84398d2007de509da5a1b05c9062128538d
gabrielweiz <gabrielweizenmann@gmail.com>
2021-09-10 16:32:44 -0300

    funcionando com comparadores

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import pymongo
 from flask import Flask, jsonify, request
-from bson import json_util
 
 client = pymongo.MongoClient(os.getenv('MONGO_URL', 'localhost'))
 db = client['movies-data']["movies"]
@@ -14,9 +13,39 @@ def health():
     return 'ok'
 
 
+"""
+Rota que aceita qualquer filtro simples passado.
+Pro caso de "stars" e "director" ele aceita um filtro composto usando "OR" entre os valores,
+Dessa forma retornando filmes que possuem um OU outro valor.
+Também é possivel filtrar os filmes com valor maior ou menor igual com os campos de número utilizando => ou <=
+
+Exemplos:
+
+http://192.168.15.74:7000/movies?director=Frank%20CapraORSteven%20Spielberg
+retorna os filmes de ambos diretores
+
+http://192.168.15.74:7000/movies?year=>2005
+retorna os filmes a partir de 2005
+
+http://192.168.15.74:7000/movies?rotten_tomatoes_critics_rating=>99
+filmes com score 99 ou mais pelos criticos do rotten tomatoes
+
+http://192.168.15.74:7000/movies?rotten_tomatoes_critics_rating=>&rotten_tomatoes_audience_rating=>95&imdb_rating=>8.4
+filmes com score >= 99 rotten_tomatoes_critics_rating >= 95 rotten_tomatoes_audience_rating e >= 8.4 score no imdb
+
+"""
 @app.route('/movies', methods=["GET"])
 def get_movies():
-    movies = list(db.find(request.args.to_dict(), {"_id": False}))
+    query = request.args.to_dict()
+    for k, v in query.items():
+        if k == "stars" or k == "director" and "OR" in v:
+            query[k] = {"$in": v.split("OR")}
+        if ">" in v or "<" in v:
+            query[k] = {"$gte" if ">" in v else "$lte": float(v[1:]) if k == "imdb_rating" else int(v[1:])}
+        elif "rating" in k or k == "year":
+            query[k] = float(v) if k == "imdb_rating" else int(v)
+
+    movies = list(db.find(query, {"_id": False}))
     return jsonify({"movies": movies, "count": len(movies)})
 
 
